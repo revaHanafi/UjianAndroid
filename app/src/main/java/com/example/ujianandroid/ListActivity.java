d2717eb119362c2382734e7c5e089d20d033812f
revaHanafi <hanafireva@gmail.com>
2023-05-19 14:59:45 +0700

    Ujian Android

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -25,6 +25,7 @@ public class ListActivity extends AppCompatActivity {
             daftar_nama.add("data masih kosong");
         }
 
+
         ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
 
         lvNama.setAdapter(ad_nama);
